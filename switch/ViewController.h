c0fd52c244c957ce14e8d9f2fca072ec53e6ada8
saikrishna <saikrishna@saikrishnas-macbook-pro.local>
2016-01-11 11:02:21 +0530

    Initial Commit

---
+++ switch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  switch
+//
+//  Created by saikrishna on 11/01/16.
+//  Copyright © 2016 saikrishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ switch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  switch
+//
+//  Created by saikrishna on 11/01/16.
+//  Copyright © 2016 saikrishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
